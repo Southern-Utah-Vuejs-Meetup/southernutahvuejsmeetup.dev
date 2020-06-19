add3928a698c7f7ae186000e3a5de9c83a6c1f2d
harwoodspike <jharwood@harwoodspike.com>
2020-06-19 14:09:06 -0600

    initial site

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ title }}</h1>
+    <p v-html="msg"></p>
+    <h2 class="coming-soon">Website coming soon!</h2>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+
+  data: () => ({
+    title: "Welcome to the Southern Utah Vue.js Meetup",
+    msg:
+      "Vue.js is a progressive JavaScript framework.<br>" +
+      "This group is to get Vue developers together to talk about a variety of topics from the most basic to the more advanced.<br>" +
+      "All skill levels are welcome, if you're interested in Vue we would love to have you join us!<br>" +
+      "This group will be doing it's events online until further notice."
+  })
 };
 </script>
+
+<style lang="scss">
+.coming-soon {
+  margin-top: 80px;
+}
+</style>
