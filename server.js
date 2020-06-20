ab9f2c75f7d5470b556103cff635f6e0e8d7d648
harwoodspike <jharwood@harwoodspike.com>
2020-06-20 15:21:59 -0600

    Add vuetify + update heroku server file

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const port = process.env.PORT || 8080;
 const dist = __dirname + "/dist/";
 
 app.set("x-powered-by", false);
 app.use(express.static(dist));
-app.use(express.json());
 
 app.get("*", (req, res) => res.sendFile(dist + "index.html"));
 app.listen(port, () => console.log(`App running on port ${port}.`));
